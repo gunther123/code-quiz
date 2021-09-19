814bfc917eb6c8d1f509627d3aaa9acf675eafc8
Steven Slocum <steven.t.slocum@gmail.com>
2021-09-19 16:44:50 -0500

    added button creation loop and button container creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,34 @@ const score = 0;
 const timer = 99;
 
 const startQuiz = function(){
-    while(pageContentEl.firstChild){
-        pageContentEl.removeChild(pageContentEl.firstChild);
-    }
+    pageContentEl.removeChild(questionContainerEl);
+    pageContentEl.removeChild(startButtonContainerEl);
 
+    createQuiz();
+
+}
+
+const createQuiz = function(){
+    const questionHeaderTextEl = document.getElementById("question-header-text");
+    questionHeaderTextEl.textContent = "What variable type allows you to store true or false values?"
+    
+
+    let buttonCount = 0;
+
+    for(buttonCount; buttonCount < 4; buttonCount++)
+    {
+      let newButton = document.createElement("button");
+      newButton.className = "btn";
+      newButton.id = "btn-" + buttonCount;
+    }
+    const btn0 = document.getElementById("btn-0");
+    const btn1 = document.getElementById("btn-1");
+    const btn2 = document.getElementById("btn-2");
+    const btn3 = document.getElementById("btn-3");
+
+    const mcButtonContainer = document.createElement("div");
+    mcButtonContainer.className = "mc-button-container";
+    mcButtonContainer.id = "mc-button-container";
 }
 
 
