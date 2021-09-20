a4d8a8f262ec2373e7cf48c5c153f69f47e57719
Steven Slocum <steven.t.slocum@gmail.com>
2021-09-19 20:39:21 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,53 @@ const questionContainerEl = document.getElementById("question-container");
 const startButtonContainerEl = document.getElementById("start-button-container")
 const startButtonEl = document.getElementById("start-btn")
 
-//variables question score and count down timer
-const score = 0;
-const timer = 99;
+//Variables question score and count down timer
+let score = 0;
+let timer = 99;
+let questionCount = 0;
 
+//Variables for questions, guesses, and answers
+const q0 = {
+    question: "What variable type allows you to store true or false values?",
+    a: "A. Boolean",
+    b: "B. Integer",
+    c: "C. String",
+    d: "D. Array",
+    answer: "a"
+}
+
+const q1 = {
+    question: "Commonly used data types DO NOT include:",
+    a: "A. Strings",
+    b: "B. Booleans",
+    c: "C. Numbers",
+    d: "D. Alerts",
+    answer: "d"
+}
+const q2 = {
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    a: 'A. <script src="xxx.js"',
+    b: 'B. <script name="xxx.js">',
+    c: 'C. <script href="xxx.js"',
+    d: 'D. None of the above',
+    answer: "a"
+}
+
+const q3 = {
+    question: "Inside which HTML element do we put the JavaScript?",
+    a: "A. <javascript>",
+    b: "B. <scripting>",
+    c: "C. <script>",
+    d: "D. <js>",
+    answer: "c"
+}
+
+//Starts the Quiz
 const startQuiz = function(){
     pageContentEl.removeChild(questionContainerEl);
     pageContentEl.removeChild(startButtonContainerEl);
-
     createQuiz();
-
+    takeQuiz();
 }
 
 const createQuiz = function(){
@@ -25,16 +62,19 @@ const createQuiz = function(){
     const btn0El = document.createElement("button");
     btn0El.className = "btn";
     btn0El.id = "btn-0";
-    btn0El.textContent = "placeholder"
+    btn0El.textContent = q0.a;
     const btn1El = document.createElement("btn-1");
     btn1El.className = "btn";
     btn1El.id = "btn-1";
+    btn1El.textContent = q0.b;
     const btn2El = document.createElement("btn-2");
     btn2El.className = "btn";
     btn2El.id = "btn-2";
+    btn2El.textContent = q0.c;
     const btn3El = document.createElement("btn-3");
     btn3El.className = "btn";
     btn3El.id = "btn-3";
+    btn3El.textContent = q0.d;
 
     const mcButtonContainerEl = document.createElement("div");
     mcButtonContainerEl.className = "mc-button-container";
@@ -50,8 +90,12 @@ const createQuiz = function(){
     mcButtonContainerEl.appendChild(btn1El);
     mcButtonContainerEl.appendChild(btn2El);
     mcButtonContainerEl.appendChild(btn3El);
+
+    questionCount++;
 }
 
 
 startButtonEl.addEventListener("click", startQuiz);
+
+
 
